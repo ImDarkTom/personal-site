d729805ad2a08113da794359798b22710f678832
ImDarkTom <darktomg21@gmail.com>
2026-06-09 04:23:30 +0100

    feat: timeline + about

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+      plugins: [tailwindcss()]
+  },
+
+  fonts: [{
+      provider: fontProviders.fontsource(),
+      name: 'Inter',
+      cssVariable: '--font-inter',
+  }],
+
+  integrations: [icon()],
 });
